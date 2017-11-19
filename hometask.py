b6e1237864adeaa68655217672423a84a3adda52
iseriki <i8000seriki@gmail.com>
2017-11-19 19:25:58 +0000

    Add hometask.py

---
+++ hometask.py
@@ -0,0 +1,4 @@
+names = ['Ann', 'Bob', 'Suzan', 'Radonir', 'Fill', 'Sam', 'Al']
+ages = [12, 33, 56, 87, 4, 18, 21]
+text_a = "Special cases aren't special enough to break the rules."
+text_b = "In the face of ambiguity, refuse the temptation to guess."
