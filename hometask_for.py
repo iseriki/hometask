8efa27003c9bcbc870c90e8eaa0a24610f30e963
iseriki <i8000seriki@gmail.com>
2017-11-22 20:48:51 +0000

    New hometask 2

--- hometask_for.py
+++ hometask_for.py
@@ -4,8 +4,9 @@ names = ['Ann', 'Fillip', 'Monika', 'Ross', 'Sam', 'Alf', 'Roy', 'Sasha', 'Leon'
 for i in range(len(names)):
 	print(names[i])
 
-
-
+#2
+for i in range(len(names)):
+	print i, names[i]
 
 
 
