b3acd845f4627c164eb5be2fc4a269272eb0609e
iseriki <i8000seriki@gmail.com>
2017-12-10 22:01:44 +0000

    Add task 'class Person'

--- classes.py
+++ classes.py
@@ -16,3 +16,21 @@ print vasily.name, vasily.age, vasily.weight, vasily.height
 print vasily.kick()
 print vasily.pull_ups()
 
+#2
+
+class Person: 
+    def __init__(self, age, name, second_name, birthday_month): 
+        self.age = age 
+        self.name = name.capitalize()
+        self.second_name = second_name.capitalize()
+        self.birthday_month = birthday_month 
+    def full_name(self):
+        return self.name + ' ' + self.second_name
+    def email(self):
+        return (self.name + '.' + self.second_name + '@email.com').lower()
+
+body = Person(age=18, name='peDro', second_name='VillALobos', birthday_month='Marth') 
+
+print body.age, body.name, body.second_name, body.birthday_month 
+print body.full_name()
+print body.email()
